45c244e2ca0592d130962748971e9fc35b805f39
Thomas Dou <tdou@carrick-99.dynamic.rpi.edu>
2023-12-03 23:10:31 -0500

    added ALU.v file

---
+++ ALU.v
@@ -0,0 +1,19 @@
+module ALU(
+    input [31:0] left,
+    input [31:0] right,
+    input [3:0] control, 
+    output [31:0] out
+);
+
+always @(left, right, control)
+    begin
+       case(control)
+        4'b0000: out = left & right; 
+        4'b0001: out = left | right; 
+        4'b0010: out = left + right; 
+        4'b0110: out = left - right; 
+        4'b0111: out = left < right; 
+        4'b1100: out = left ~| right; 
+       endcase
+    end   
+endmodule
