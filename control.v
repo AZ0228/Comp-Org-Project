a3e440770f47f3ee2076c78a0f9ae17748a44f35
Thomas Dou <tdou@folsom-wl-84.dynamic2.rpi.edu>
2023-12-07 16:56:41 -0500

    modified ALU.v file and control.v file

--- control.v
+++ control.v
@@ -1,9 +1,31 @@
-module controlunit (input [4:0] opcode, 
-output [4:0] control_signal)
+module controlunit (input [3:0] opcode, 
+output reg [0:0] RegDst,
+output reg [0:0] RegWrite,
+output reg [0:0] ALUSrc,
+output reg [0:0] ALUOp,
+output reg [3:0] placeholder);
 
-    always @(opcode) begin
+always @(*) 
+    begin
         case (opcode)
-            
+            /*
+            opcodes
+
+            add - 0
+            halt - 1
+            load - 2
+            store - 3
+            clear - 4
+            skip - 5
+            jump - 6
+            */            
+            6'b00000: begin
+                RegDst = 1'b1;
+            end
+            6'b100011: placeholder = 4'b0000;
+            6'b101011: placeholder = 4'b0000;
+            //branch
+            6'b000100: placeholder = 4'b0000;
         endcase 
     end
 
