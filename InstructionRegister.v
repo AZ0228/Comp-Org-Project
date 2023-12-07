1cec16cc0980ac3b608d5a0fbd08eed86e4cf565
Kaya <kyaajergin@gmail.com>
2023-12-07 15:16:18 -0500

    modified InstructionRegister.v

--- InstructionRegister.v
+++ InstructionRegister.v
@@ -2,19 +2,26 @@ module instruction_register(
 	input clock,
 	input reset,
 	input [31:0] register_input,
-	output reg [6:0] opcode,
+	output reg [5:0] opcode,
 	output reg [24:0] target_address,
-	reg [31:0] temp,
 	);
+
+	reg [31:0] temp;
+	reg sync_reset;
+
+	always @(posedge clock or posedge reset)
+	begin
+		if (reset)
+			sync_reset <= 1;
+		else
+			sync_reset <= 0;
+		if (sync_reset)
+			temp <= register_input;
+	end
 
 	always @(posedge clock)
 	begin
-			if (reset == 1) begin
-			temp <= register_input;
-			end
-	opcode <= temp[31:25];
-	target_address <= temp[24:0];
+		opcode <= register_input[31:25];
+		target_address <= register_input[24:0];
 	end
 endmodule
-
-
