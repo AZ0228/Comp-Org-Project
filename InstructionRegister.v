9dbe72308cdd571a97e33b7d335916d53396c81f
Kaya <kyaajergin@gmail.com>
2023-12-07 16:48:39 -0500

    changed instruction register to work with 4-bit opcode

--- InstructionRegister.v
+++ InstructionRegister.v
@@ -2,8 +2,8 @@ module instruction_register(
 	input clock,
 	input reset,
 	input [31:0] register_input,
-	output reg [5:0] opcode,
-	output reg [24:0] target_address,
+	output reg [3:0] opcode, // 4 bit opcode
+	output reg [29:0] target_address,
 	);
 
 	reg [31:0] temp;
@@ -21,7 +21,7 @@ module instruction_register(
 
 	always @(posedge clock)
 	begin
-		opcode <= register_input[31:25];
-		target_address <= register_input[24:0];
+		opcode <= register_input[31:28];
+		target_address <= register_input[29:0];
 	end
 endmodule
